914a590c6b4c7bc407c73164fd1b58d972296bbb
tatjanon <winfield@support.email.ne.jp>
2025-05-11 10:01:07 +0900

    Rename -scratch option to -offline : CalcVmDsSizeReq.ps1

--- CalcVmDsSizeReq.ps1
+++ CalcVmDsSizeReq.ps1
@@ -4,13 +4,13 @@
 
  .DESCRIPTION
   This script calculates the required Datastore size based on the intended memory allocation and 
-  occupancy rate. If the VM does not exist yet (indicated by the -scratch switch), the calculation is 
+  occupancy rate. If the VM does not exist yet (indicated by the -offline switch), the calculation is 
   based on user-specified disk size and memory size.
-  Version: 1.1.1
+  Version: 1.1.2
 
   The script supports both existing VMs (by connecting to a vCenter server) and hypothetical scenarios 
   without requiring a working vSphere environment. For existing VMs, it retrieves details such as current 
-  memory, Datastore name and disk usage. For non-existent VMs (using -scratch), the user provides the 
+  memory, Datastore name and disk usage. For non-existent VMs (using -offline), the user provides the 
   necessary inputs for the calculation.
 
   The calculation uses the formula:
@@ -19,23 +19,23 @@
   Notes:
   - The script does not account for temporary files (e.g., log files and snapshots), making it suitable for high-level storage planning.
   - Default Datastore occupancy rate is 80% if not provided by the user.
-  - Adjust the "vCenter connection info" section for your environment. These settings are ignored in -scratch mode.
+  - Adjust the "vCenter connection info" section for your environment. These settings are ignored in -offline mode.
 
  .PARAMETER MemorySize
   (Alias -m) Mandatory. Intended future size of memory in GB.
 
  .PARAMETER VmName
-  (Alias -n) Mandatory unless -scratch is specified. Specifies the VM name.
+  (Alias -n) Mandatory unless -offline is specified. Specifies the VM name.
 
  .PARAMETER OccupancyRate
   (Alias -r) Optional. A number between 0 and 1 (e.g., 0.75) representing the desired maximum Datastore 
   occupancy rate. Defaults to 0.8 if not provided.
 
- .PARAMETER scratch
+ .PARAMETER offline
   Switch. Indicates that the VM does not exist yet. Must be used with -DiskSize.
 
  .PARAMETER DiskSize
-  (Alias -d) Mandatory when -scratch is specified. Specifies the total size of VM disks in GB.
+  (Alias -d) Mandatory when -offline is specified. Specifies the total size of VM disks in GB.
 
  .EXAMPLE
   # Calculate the required Datastore size for an existing VM after expanding its memory to 16GB.
@@ -45,7 +45,7 @@
  .EXAMPLE
   # Estimate the Datastore size for an imaginary VM with 10GB of memory and a total disk size of 200GB, assuming the total size (disk + memory swap) will fill 85% of the Datastore.
   # Does not require a vCenter connection. You can optionally add '-VmName NewVM002' for clarity or as a reminder.
-  .\CalcVmDsSizeReq.ps1 -MemorySize 10 -scratch -DiskSize 200 -OccupancyRate 0.85
+  .\CalcVmDsSizeReq.ps1 -MemorySize 10 -offline -DiskSize 200 -OccupancyRate 0.85
 #>
 [CmdletBinding()]
 Param(
@@ -62,7 +62,7 @@ Param(
   [double]$OccupancyRate,
 
   [Parameter()]
-  [switch]$scratch,
+  [switch]$offline,
 
   [Parameter()]
   [Alias("d")]
@@ -91,13 +91,13 @@ begin {
         throw "Error: -MemorySize is a mandatory parameter and must be specified."
     }
 
-    if ($PSBoundParameters.ContainsKey('scratch')) {
+    if ($PSBoundParameters.ContainsKey('offline')) {
         if (-not $PSBoundParameters.ContainsKey('DiskSize')) {
-            throw "Error: -DiskSize must be specified when using -scratch."
+            throw "Error: -DiskSize must be specified when using -offline."
         }
     } else {
         if (-not $PSBoundParameters.ContainsKey('VmName')) {
-            throw "Error: -VmName is a mandatory parameter unless -scratch is specified."
+            throw "Error: -VmName is a mandatory parameter unless -offline is specified."
         }
     }
 
@@ -135,7 +135,7 @@ process {
         }
     }
 
-    if (! $scratch) {
+    if (! $offline) {
         if (-not (Get-Module VMware.VimAutomation.Core)) {
             Write-Output "Loading VMware PowerCLI module, this may take a while..."
             Import-Module VMware.VimAutomation.Core -ErrorAction SilentlyContinue
@@ -183,7 +183,7 @@ process {
     Write-Host "VM Name              : $VmName"
     Write-Host "Current Memory Size  : $currentMemoryGB GB"
     Write-Host "New Memory Size      : $MemorySize GB"
-    if ($scratch) {
+    if ($offline) {
         Write-Host "New Disk Size        : $currentDisksGB GB"
     } else {
         Write-Host "Current Disk Size    : $currentDisksGB GB"
