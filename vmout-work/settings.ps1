49fe541d3122132b0b456f95fecfdf44d15925dc
tatjanon <winfield@support.email.ne.jp>
2026-03-17 13:04:56 +0900

    Refactor vCenter connection settings in settings.ps1
    * Added '$Legacy' parameter
    * Reordered some parameters to clarify the framework and purpose of each setting variables

--- vmout-work/settings.ps1
+++ vmout-work/settings.ps1
@@ -5,48 +5,56 @@ $vcserver = 'vcsa1.mydomain.com'
 $vcport = 443
 
 # vCenter login user
-# NOTE:
 #   - $vcuser should normally NOT be commented out.
 #   - This script expects a user name to always be provided.
 $vcuser  = 'Administrator@vsphere.local'
 
 # Optional: vCenter password (plain text)
-#   - If set, the script uses legacy plain password mode:
-#       Connect-VIServer -User $vcuser -Password $vcpasswd
-#   - It is not recommended for production site from a security perspective
-#     though it may be convenient in lab / build environments.
+#   - If set, the script uses plain password mode:
+#   - It is not recommended for production site from a security perspective.
 #$vcpasswd = 'ChangeMe!'
 
-#   - When using SecretStore / VISecret for credential management, keep the
-#     above $vcpasswd line commented out, and:
-#     - Install the Microsoft.PowerShell.SecretManagement and
-#       Microsoft.PowerShell.SecretStore modules if they are missing
-#       (both PowerShell 7 and Windows PowerShell 5.1+ are supported).
-#     - Download Modules/VISecret from GitHub:
-#         @vmware-archive/PowerCLI-Example-Scripts
-#       and place it under a PSModulePath location as VMware.VISecret.
-#     - Then, in a PowerShell console, run:
-#         Import-Module VMware.VISecret
-#         Initialize-VISecret -Vault "VMwareSecretStore"
-#       (the password for $vcuser@$vcserver will be prompted at the first
-#       successful login and stored in the SecretVault)
+# Optional: Legacy VICredentialStore mode
+#   - If set to positive (true/True/yes/Yes) and $vcpasswd is empty,
+#     script uses Legacy VICredentialStore mode by default,
+#     so script's option "-Legacy" can be omitted at each run.
+#$Legacy = true
+#   - The password for $vcuser@$vcserver will be prompted at the first
+#     login attempt and stored in the VICredentialStore if the login is
+#     successful.
+
+# When using SecretStore / VISecret credential management mode:
+#   - Keep $vcpasswd line commented out and set $Legacy to negative or
+#     comment it out either.
+#   - Install the Microsoft.PowerShell.SecretManagement and
+#     Microsoft.PowerShell.SecretStore modules if they are missing
+#     (both PowerShell 7 and Windows PowerShell 5.1+ are supported).
+#   - Download Modules/VISecret from GitHub:
+#       @vmware-archive/PowerCLI-Example-Scripts
+#     and place it under a PSModulePath location as VMware.VISecret.
+#   - Then, to prepare the Secret Vault, in a PowerShell console, run:
+#       Import-Module VMware.VISecret
+#       Initialize-VISecret -Vault "VMwareSecretStore"
+#     (see also $VaultDefault and $Vault below)
+#   - The password for $vcuser@$vcserver will be prompted at the first
+#     login attempt and stored in the SecretVault if the login succeeds.
+
+# Optional: SecretManagement / VISecret vault name
+#   - These variables are used only in non-plain, SecretStore mode. Legacy
+#     VICredentialStore mode is not also the case, so can be left untouched.
+#   - If $Vault is NOT defined here, the script falls back to
+#     $VaultDefault (typically "VMwareSecretStore").
+#   - NOTE:
+#       * $VaultDefault corresponds to the default used by Initialize-VISecret
+#         in the VMware.VISecret module at the time of writing.
+#       * Future versions of VISecret may change that default. For stable
+#         production use, it is RECOMMENDED to explicitly set $Vault and
+#         $VaultDefault here instead of relying on the module's default.
+$VaultDefault = 'VMwareSecretStore'
+$Vault        = 'VMwareSecretStore'
 
 # Number of connection retries when connecting to vCenter fails
 $connRetry = 2
 
 # Connection retry interval in seconds
 $connRetryInterval = 5
-
-# Optional: SecretManagement / VISecret vault name
-#   - If $vcpasswd is NOT set, the script will try to use SecretStore / VISecret.
-#   - If $Vault is NOT defined here, the script falls back to $VaultDefault
-#     defined in the script itself (typically "VMwareSecretStore").
-#   - NOTE:
-#       * $VaultDefault corresponds to the default used by Initialize-VISecret
-#         in the VMware.VISecret module at the time of writing.
-#       * Future versions of VISecret may change that default. For stable,
-#         production use it is RECOMMENDED to explicitly set $Vault here instead
-#         of relying on the script default.
-#   - In plain password mode, $Vault variable below will never be used.
-$VaultDefault = 'VMwareSecretStore'
-$Vault        = 'VMwareSecretStore'
